59f4366cc10d4caf53b525b7475b5abb68408964
SophieZhao00 <zhaoxuefei@utexas.edu>
2020-06-03 21:25:27 +0000

    Add feature: show comments input after user login

--- portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/LoginServlet.java
@@ -17,6 +17,7 @@ package com.google.sps.servlets;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import java.io.IOException;
+import java.io.PrintWriter;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -28,21 +29,23 @@ public class LoginServlet extends HttpServlet {
     @Override
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
         response.setContentType("text/html");
-
         UserService userService = UserServiceFactory.getUserService();
-        if (userService.isUserLoggedIn()) {
-            String userEmail = userService.getCurrentUser().getEmail();
-            String urlToRedirectToAfterUserLogsOut = "/login";
-            String logoutUrl = userService.createLogoutURL(urlToRedirectToAfterUserLogsOut);
 
-            response.getWriter().println("<p>Hello " + userEmail + "!</p>");
+        // Only logged-in users can see the form
+        if (userService.isUserLoggedIn()) {
+            String logoutUrl = userService.createLogoutURL("/index.html");
+
+            response.getWriter().println("<form action=\"/data\" method=\"POST\">");
+            response.getWriter().println("<textarea name=\"text-input\"></textarea>");
+            response.getWriter().println("<br/><br/>");
+            response.getWriter().println("<button>Submit</button>");
+            response.getWriter().println("</form>");
+            
             response.getWriter().println("<p>Logout <a href=\"" + logoutUrl + "\">here</a>.</p>");
         } else {
-            String urlToRedirectToAfterUserLogsIn = "/login";
-            String loginUrl = userService.createLoginURL(urlToRedirectToAfterUserLogsIn);
+            String loginUrl = userService.createLoginURL("/comment.html");
 
-            response.getWriter().println("<p>Hello stranger.</p>");
-            response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a>.</p>");
+            response.getWriter().println("<p>Login <a href=\"" + loginUrl + "\">here</a> to leave a comment.</p>");
         }
     }
 }
