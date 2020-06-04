4f3e96d72fced4b8c0313d3470804f53f339d9fb
SophieZhao00 <zhaoxuefei@utexas.edu>
2020-06-04 23:22:21 +0000

    Implement algorithm to get a collection of time ranges that are able to schedule meeting among attendees.

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,82 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.ArrayList;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+
+        // get TimeRange of events filtered by attendees
+        List<TimeRange> eventsTimeRanges = new ArrayList<>();
+        for (Event event: events) 
+            if(containSameAttendees(event, request))
+                eventsTimeRanges.add(event.getWhen());
+
+        Collections.sort(eventsTimeRanges, TimeRange.ORDER_BY_START);
+        Collection<TimeRange> combinedTimeRanges = combineTimeRanges(eventsTimeRanges);
+
+        return getFreeTimeRanges(combinedTimeRanges, request);
+    }
+
+    /** Check if the event and the request contains the same attendee. */
+    private boolean containSameAttendees(Event event, MeetingRequest request) {
+        for (String attendee: event.getAttendees())
+            if (request.getAttendees().contains(attendee))
+                return true;
+        return false;
+    }
+
+
+    /**
+     * Combine overlapped TimeRanges in a sorted List.
+     * @return a sorted Collection of TimeRanges without overlap.
+     */
+    private Collection<TimeRange> combineTimeRanges(List<TimeRange> timeRanges) {
+        Collection<TimeRange> combinedTimeRanges = new ArrayList<>();
+
+        if (timeRanges.size() == 0)
+            return combinedTimeRanges;
+        
+        TimeRange currentEvent = timeRanges.get(0);
+        for (int i = 1; i < timeRanges.size(); i++) {
+            TimeRange nextEvent = timeRanges.get(i);
+            if (currentEvent.overlaps(nextEvent)) {
+                // update current TimeRange
+                int start = currentEvent.start();
+                int end = currentEvent.contains(nextEvent.end())? currentEvent.end() : nextEvent.end();
+                currentEvent = TimeRange.fromStartEnd(start, end, false);
+            } else {
+                // store current TimeRange
+                combinedTimeRanges.add(currentEvent);
+                currentEvent = nextEvent;
+            }
+        }
+        combinedTimeRanges.add(currentEvent);
+
+        return combinedTimeRanges;
+    }
+
+
+    /**
+     * Get a Collection of free TimeRanges given a collection of busy TimeRanges.
+     * Only include TimeRanges with at least requested duration.
+     */
+    private Collection<TimeRange> getFreeTimeRanges(Collection<TimeRange> busyTimeRanges, MeetingRequest request) {
+        Collection<TimeRange> freeTimeRanges = new ArrayList<>();
+        int start = TimeRange.START_OF_DAY;
+        for (TimeRange time: busyTimeRanges) {
+            int newEnd = time.start();
+            int newStart = time.end();
+            if (newEnd - start >= request.getDuration())
+                freeTimeRanges.add(TimeRange.fromStartEnd(start, newEnd, false));
+            start = newStart;
+        }
+
+        if (TimeRange.END_OF_DAY - start >= request.getDuration())
+            freeTimeRanges.add(TimeRange.fromStartEnd(start, TimeRange.END_OF_DAY, true));
+
+        return freeTimeRanges;
+    }
 }
